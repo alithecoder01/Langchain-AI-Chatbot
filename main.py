c8c075bd3f61c343ff65db59f7af3bb9c1de0f7f
Ali Hasan <capt.3lii@gmail.com>
2024-01-01 10:28:02 +0300

    Connection established but there is an error in number of quots

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from langchain.llms import openai
+from langchain.llms import OpenAI
 from dotenv import load_dotenv
 
+load_dotenv()
+
+llm = OpenAI(temperature= 0.7)
+
+name = llm("I have a dog, can you suggest 5 names for it ?")
+
+print(name)
