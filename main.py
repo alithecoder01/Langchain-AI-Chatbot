8b9e6ad476dd95b1618fdfc228ef191b89c0559e
Ali Hasan <capt.3lii@gmail.com>
2024-01-02 11:20:47 +0300

    Add more input variables intp the prompts

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def genratename(animal_type):
+def genratename(animal_type, pet_color):
     llm = OpenAI(temperature= 0.7)
 
     promt_temp_name = PromptTemplate(
-        input_variables=['animal_type'],
-        template="I have a {animal_type}, can you suggest 5 names for it ?"
+        input_variables=['animal_type', 'pet_color'],
+        template="I have a {animal_type}, it's {pet_color} in color. Can you suggest 5 names for it ?"
     )
     name_chain= LLMChain(llm= llm, prompt= promt_temp_name)
 
-    respons = name_chain({'animal_type': animal_type})
+    respons = name_chain({'animal_type': animal_type, 'pet_color': pet_color})
 
     return respons
 
 if __name__ == "__main__":
-    print(genratename("cat"))
+    print(genratename("cat","red"))
