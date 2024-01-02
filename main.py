48dd4415023c2811e1fa8c723a8f62740ac89e0e
Ali Hasan <capt.3lii@gmail.com>
2024-01-02 11:17:02 +0300

    connection done, do the promopt way to make it not hardcoded

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
-llm = OpenAI(temperature= 0.7)
+def genratename(animal_type):
+    llm = OpenAI(temperature= 0.7)
 
-name = llm("I have a dog, can you suggest 5 names for it ?")
+    promt_temp_name = PromptTemplate(
+        input_variables=['animal_type'],
+        template="I have a {animal_type}, can you suggest 5 names for it ?"
+    )
+    name_chain= LLMChain(llm= llm, prompt= promt_temp_name)
 
-print(name)
+    respons = name_chain({'animal_type': animal_type})
+
+    return respons
+
+if __name__ == "__main__":
+    print(genratename("cat"))
