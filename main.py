fd421bad5affb80e2b8676e5a056d1030b5c19b9
Ali Hasan <capt.3lii@gmail.com>
2024-01-03 11:29:46 +0300

    Set the page sidebar with the element we have

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import streamlit as st
 
 
 st.title("Pets name genrator")
+
+# Select the Animal type 
+animal_type = st.sidebar.selectbox("What is your pet ?", ("Cat", "Cow", "Dog"))
+
+# Select the color
+if animal_type == "Cat":
+    pet_color =st.sidebar.text_area(label="What is your Pet Color?", max_chars=10) 
+if animal_type == "Cow":
+    pet_color =st.sidebar.text_area(label="What is your Pet Color?", max_chars=10) 
+if animal_type == "Dog":
+    pet_color =st.sidebar.text_area(label="What is your Pet Color?", max_chars=10) 
